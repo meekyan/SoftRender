52af48d08964ac8d11e58e1c143f62455f1beef6
meekyan <meekyan@123u.com>
2019-09-07 10:57:20 +0800

    Martix

--- YSoftRender/YSoftRender/Martix.cpp
+++ YSoftRender/YSoftRender/Martix.cpp
@@ -1,3 +1,61 @@
-//#include "Martix.h"
-//
 #include "pch.h"
+#include "Martix.h"
+
+void Martix::SetZero()
+{
+	for (int i = 0; i < 4; ++i)
+		for (int j = 0; j < 4; ++j)
+			martix[i][j] = 0.0f;
+}
+
+Martix Martix::operator+(const Martix& t) const
+{
+	Martix result;
+	for (int i = 0; i < 4; ++i)
+		for (int j = 0; j < 4; ++j)
+			result.martix[i][j] = martix[i][j] + t.martix[i][j];
+	return result;
+}
+
+Martix Martix::operator-(const Martix& t) const
+{
+	Martix result;
+	for (int i = 0; i < 4; ++i)
+		for (int j = 0; j < 4; ++j)
+			result.martix[i][j] = martix[i][j] - t.martix[i][j];
+	return result;
+}
+
+Martix Martix::operator*(const Martix& t) const
+{
+	Martix result;
+	for (int i = 0; i < 4; ++i)
+		for (int j = 0; j < 4; ++j)
+			for (int k = 0; k < 4; ++k)
+				result.martix[i][j] += martix[j][k] * t.martix[k][i];
+	return result;
+}
+
+void Martix::identity()
+{
+	for (int i = 0; i < 4; ++i)
+		for (int j = 0; j < 4; ++i)
+			martix[i][j] = 0.0f;
+	for (int i = 0; i < 4; ++i)
+		martix[i][i] = 1.0f;
+}
+
+bool Martix::operator== (const Martix& t) const
+{
+	for (int i = 0; i < 4; ++i)
+		for (int j = 0; j < 4; ++j)
+			if (abs(martix[i][j] - t.martix[i][j]) >= 0.000001f)
+				return false;
+	return true;
+}
+
+Martix Martix::operator^ (const Martix& t) const
+{
+	Martix result;
+	return result;
+}
--- YSoftRender/YSoftRender/Martix.h
+++ YSoftRender/YSoftRender/Martix.h
@@ -4,18 +4,53 @@
 class Martix
 {
 public:
-	//单位化
-	void Identity();
-	//0矩阵
-	void SetZero();
-	//等于
-	bool operator== (const Martix& rhs) const;
-	//加
-	Martix operator+ (const Martix& rhs) const;
-	//减
-	Martix operator- (const Martix& rhs) const;
-	//乘
-	Martix operator* (const Martix& rhs) const;
-private:
+	union
+	{
+		float martix[4][4];
+		struct
+		{
+			float _00; float _01; float _02; float _03;
+			float _10; float _11; float _12; float _13;
+			float _20; float _21; float _22; float _23;
+			float _30; float _31; float _32; float _33;
+		};
+	};
 
+	Martix() = default;
+	Martix(float a0, float a1, float a2, float a3,
+		float b0, float b1, float b2, float b3,
+		float c0, float c1, float c2, float c3,
+		float d0, float d1, float d2, float d3)
+	{
+		_00 = a0; _01 = a1;_02 = a2;_03 = a3;
+		_10 = b0; _11 = b1;_12 = b2;_13 = b3;
+		_20 = c0; _21 = c1;_22 = c2;_23 = c3;
+		_30 = d0; _31 = d1;_32 = d2;_33 = d3;
+	}
+	Martix(const Martix& t)
+	{
+		for (int i = 0; i < 4; ++i)
+			for (int j = 0; j < 4; ++j)
+				martix[i][j] = t.martix[i][j];
+	}
+	Martix& operator= (const Martix& t)
+	{
+		for (int i = 0; i < 4; ++i)
+		{
+			for (int j = 0; j < 4; ++j)
+			{
+				martix[i][j] = t.martix[i][j];
+			}
+		}
+		return *this;
+	}
+
+	void SetZero();//0矩阵
+	bool operator== (const Martix& t) const;//判相等
+	Martix operator+ (const Martix& t) const;//相加
+	Martix operator- (const Martix& t) const;//减
+	Martix operator* (const Martix& t) const;//乘法
+	Martix operator^ (const Martix& t) const;//幂
+	void identity();//单位矩阵
+private:
 };
